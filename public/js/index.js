bec17659fc55357b546bf701ca427fbf5dd37b93
ChalkPE <chalkpe@gmail.com>
2015-12-13 23:01:02 +0900

    Different colors for username

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,43 @@
 var socket = io.connect();
 var username = prompt("누구세요?") || ('user' + Math.floor(Math.random() * 10000));
 
-//var colors = [ "#E91E63", "#9C27B0", "#3F51B5", "#009688", "#8BC34A", "#FFC107", "#795548", "#607D8B" ];
+function shuffleArray(array){
+    return array.map(function(element){
+        return [Math.random(), element];
+    }).sort().map(function(element){
+        return element[1];
+    });
+}
+
+function hashCode(str){
+    return str.split("").reduce(function(a,b){ a = ((a << 5) - a) + b.charCodeAt(0); return a & a; }, 0);
+}
+
+/**
+ * @todo Add other colors
+ * @see https://www.google.com/design/spec/style/color.html#color-color-palette
+ */
+var colors = shuffleArray(["pink", "purple", "indigo", "teal", "green", "amber", "brown", "grey"]);
 
 function appendMessage(message){
     $("#chatRoom").append('<li class="list-group-item">' + message + '</li>');
     $("html, body").animate({ scrollTop: $(document).height() }, "slow");
 };
 
+function createUsernameSpan(username){
+    return '<span class="username color-' + colors[Math.abs(hashCode(username)) % colors.length] + '">' + username + '</span>'
+}
+
 function onMessage(data){
-    appendMessage('<span class="username">' + data.username + '</span> ' + $('<span>').text(data.message).html());
+    appendMessage(createUsernameSpan(data.username) + ' ' + $('<span>').text(data.message).html());
 }
 
 function onJoinMessage(data){
-    appendMessage('<span class="username">' + data + '</span>님이 입장하셨습니다.');
+    appendMessage(createUsernameSpan(data) + '님이 입장하셨습니다.');
 }
 
 function onLeftMessage(data){
-    appendMessage('<span class="username">' + data + '</span>님이 퇴장하셨습니다.');
+    appendMessage(createUsernameSpan(data) + '님이 퇴장하셨습니다.');
 }
 
 socket.emit('login', {username: username });
